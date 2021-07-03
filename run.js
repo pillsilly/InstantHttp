e78c890c1f647c78f6f24e9778d6271db0800d13
pillsilly <marcfrances@outlook.com>
2021-07-03 11:42:01 +0800

    updates

--- run.js
+++ run.js
@@ -6,10 +6,10 @@ const fs = require('fs');
 const ChromeLauncher = require('chrome-launcher');
 const compression = require('compression');
 
-module.exports = function run({port = 9090, dir = process.cwd(), proxyTarget, proxyPattern, open = true}) {
+module.exports = function run({ port = 9090, dir = process.cwd(), proxyTarget, proxyPattern, open = true }) {
     const app = express();
     if (proxyTarget && proxyPattern) {
-        const {createProxyMiddleware} = require('http-proxy-middleware');
+        const { createProxyMiddleware } = require('http-proxy-middleware');
         app.use(proxyPattern, createProxyMiddleware({ target: proxyTarget, changeOrigin: true, secure: false }));
     }
     const router = express.Router();
@@ -17,23 +17,21 @@ module.exports = function run({port = 9090, dir = process.cwd(), proxyTarget, pr
         console.log(`Incoming request: ${req.originalUrl}`);
         next();
     });
-    function _400handler (err, req, res, next) {
-        console.error(err.stack)
-        res.status(404).send('404!')
-    }
+
     console.log(`Serving dir [${dir}]`);
-    app.use([cors(), compression(), router, express.static(dir), _400handler]);
-    console.log(`Serving on port ${port}`);
+    console.log(`Serving port ${port}`);
+
+    app.use([cors(), compression(), router, express.static(dir), handle404]);
 
     app.use(function (req, res, next) {
-        const testFilePath = path.resolve(`${dir}${req.url}`);
+        const requestPath = path.resolve(`${dir}${req.url}`);
 
-        fs.readdir(testFilePath, { withFileTypes: true }, (a, list) => {
+        fs.readdir(requestPath, { withFileTypes: true }, (a, list) => {
             if (!list) {
-                res.status(404).send(`resource not found: ${testFilePath}`);
+                res.status(404).send(`resource not found: ${requestPath}`);
                 return;
             };
-            const title = `<h2>Current Dir: ${testFilePath}</h2>`;
+            const title = `<h2>Current Dir: ${requestPath}</h2>`;
             const html = title + list
                 .map(f => f.name)
                 .map(name => `<a href='${name}'>${name}</a>`).join('<br>');
@@ -53,16 +51,22 @@ module.exports = function run({port = 9090, dir = process.cwd(), proxyTarget, pr
         }
     });
 
-  if (open !== true) return;
-    ChromeLauncher.launch({
-        startingUrl: `http://127.0.0.1:${port}/`,
-        chromeFlags: ['--disable-web-security']
-    }).catch(e => {
-        console.log('failed to launch chrome, pls start chrome with the flag \'--disable-web-security\' manually.', e);
-    }).then(() => {
-        process.on('exit', (code) => {
-            console.log(`Exit and kill launched chrome`);
-            ChromeLauncher.killAll();
-        })
-    });
+    if (open) {
+        ChromeLauncher.launch({
+            startingUrl: `http://127.0.0.1:${port}/`,
+            chromeFlags: ['--disable-web-security']
+        }).catch(e => {
+            console.log('failed to launch chrome, pls start chrome with the flag \'--disable-web-security\' manually.', e);
+        }).then(() => {
+            process.on('exit', (code) => {
+                console.log(`Exit and kill launched chrome`);
+                ChromeLauncher.killAll();
+            })
+        });
+    }
 };
+
+function handle404(err, req, res, next) {
+    console.error(err.stack)
+    res.status(404).send('404!')
+}
