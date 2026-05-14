a360f93c5e8d2f629f834fc4569ed6c996020355
pillsilly <marcfrances@outlook.com>
2026-05-14 16:46:10 +0800

    complex_proxy

--- test/bin.spec.ts
+++ test/bin.spec.ts
@@ -1,16 +1,19 @@
-import { run } from '../src/run';
-import { getOptions } from '../src/bin';
-jest.mock('../src/run');
+import {getOptions} from '../src/bin'
 
-describe.only('Test bin.js', function () {
+describe('Test bin.js', function () {
+  const originalArgv = process.argv
+
   beforeEach(function () {
-    // @ts-ignore
-    run.mockImplementation(() => {});
-  });
-  afterAll(function () {});
+    process.argv = ['node', 'instant_http']
+  })
+
+  afterEach(function () {
+    process.argv = originalArgv
+    jest.restoreAllMocks()
+  })
 
   it('should give default options', async function () {
-    const res = getOptions();
+    const res = getOptions()
     expect(res).toEqual(
       expect.objectContaining({
         dir: expect.stringContaining('InstantHttp'),
@@ -19,6 +22,32 @@ describe.only('Test bin.js', function () {
         port: '9090',
         quiet: false
       })
-    );
-  });
-});
+    )
+  })
+
+  it('should parse the new proxy flags', async function () {
+    process.argv = [
+      'node',
+      'instant_http',
+      '--proxyStaticFileWise',
+      '--https',
+      '--httpsKey',
+      '/tmp/key.pem',
+      '--httpsCert',
+      '/tmp/cert.pem',
+      '--proxyTarget',
+      'http://127.0.0.1:1234'
+    ]
+
+    const res = getOptions()
+    expect(res).toEqual(
+      expect.objectContaining({
+        proxyStaticFileWise: true,
+        https: true,
+        httpsKey: '/tmp/key.pem',
+        httpsCert: '/tmp/cert.pem',
+        proxyTarget: 'http://127.0.0.1:1234'
+      })
+    )
+  })
+})
